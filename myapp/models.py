bd383e067f4d2e375d46182ddd0be6e436e61016
David19 <davidleonel911@gmail.com>
2025-10-14 20:19:54 -0500

    intro a modelos y bdd

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Proyecto(models.Model): 
+    nombre = models.CharField(max_length=20)
+
+class Tareas(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.TextField()
+    proyeto = models.ForeignKey(Proyecto, on_delete=models.CASCADE) #Relacion a otra tabla, eliminar cuando se borre un proyecto se borren las tareas que estaban con el 
+
