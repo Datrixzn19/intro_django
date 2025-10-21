63e654feacd6072c221910c0f6ad468b6fac7686
David19 <davidleonel911@gmail.com>
2025-10-20 19:27:31 -0500

    uso de django admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Tareas, Proyecto
+#desde aqui podemos agregar modelos al panel de administracion 
+
 # Register your models here.
+admin.site.register(Proyecto)
+admin.site.register(Tareas)
--- myapp/models.py
+++ myapp/models.py
@@ -6,8 +6,16 @@ from django.db import models
 class Proyecto(models.Model): 
     nombre = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.nombre #con esto ya no va a aparecer Proyecto Object. sino el nombre real que tengan 
+
+
+
 class Tareas(models.Model):
     nombre = models.CharField(max_length=20)
     descripcion = models.TextField()
     proyeto = models.ForeignKey(Proyecto, on_delete=models.CASCADE) #Relacion a otra tabla, eliminar cuando se borre un proyecto se borren las tareas que estaban con el 
 
+    def __str__(self):
+        #return 'Tarea: '+ self.nombre + 'royecto: '+ self.proyeto.nombre #como proyecto es un objeto entonces podemos acceder a sus propiedades 
+        return f"TAREA: {self.nombre}    PROYECTO: {self.proyeto.nombre}" #ambas maneras funcionarion 
